2e16ca02d21621c4cd6d164c8583de0a7ac4ec2a
Mariah Morales <mjustine622@gmail.com>
2021-05-27 15:13:11 -0400

    commit tree ad7206a3cc1d1eeb47e0519392e0aade2afddc50

--- db/migrate/01_create_landmarks.rb
+++ db/migrate/01_create_landmarks.rb
@@ -1,3 +1,13 @@
 class CreateLandmarks < ActiveRecord::Migration
-  raise 'Write CreateLandmarks migration here'
-end
+  def change
+    create_table :landmarks do |t|
+      t.string :name
+      t.integer :year_completed
+      t.integer :figure_id
+    end
+  end 
+end 
+
+
+# 'landmark[name]'
+# 'landmark[year_completed]'
